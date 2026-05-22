b8f8a23d6fae81348fcc284806fc1e89f74f018a
chris9100h <kolabaeume.flops.9n@icloud.com>
2026-05-22 09:18:06 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'zane-v1.543';
+const CACHE = 'zane-v1.544';
 const CDN_HOSTS = ['unpkg.com', 'cdnjs.cloudflare.com', 'fonts.googleapis.com', 'fonts.gstatic.com'];
 const ASSETS = [
   '/training/',
